542f7ddaa4ef56aa232e47f4e38f3e0f81f1079b
PawełAndrys <pawelandrys@gmail.com>
2025-03-20 15:21:58 +0100

    init1

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { createCamera } from './src/camera.js';
 import { createRenderer } from './src/renderer.js';
 // import { setupControls } from './src/controls.js';
 import { setupLighting } from './src/lighting.js';
+import { sizes, setupResize } from "./src/resize"; // Import z resize.js
 
 const scene = createScene();
 const camera = createCamera();
@@ -10,7 +11,7 @@ const renderer = createRenderer();
 
 setupLighting(scene);
 // setupControls(scene, camera); // Pass the scene to controls
-
+setupResize(camera, renderer);
 const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera); // Render the scene from the perspective of the camera
